b07bd8a1813fafadfb7fdd3c8401f7ab54379392
rittima <rittimabiswas@gmail.com>
2025-08-04 13:19:36 +0530

    second commit

--- home.js
+++ home.js
@@ -5,28 +5,3 @@ function toggleMenu() {
 function toggleNav() {
     document.querySelector('.nav-link').classList.toggle('open');
 }
-
-const filters = document.querySelectorAll(".sidenav-link li");
-const cards = document.querySelectorAll(".card");
-
-filters.forEach(filter=>{
-    filter.addEventListener('click',()=>{
-        const select = filter.textContent.toLowerCase();
-        
-        let category = "";
-        if (select === "mobiles") category = "phone";
-        else if (select === "earphones") category = "headphone";
-        else if (select === "speakers") category = "speaker";
-
-        cards.forEach(item=>{
-            const img = item.querySelector("img");
-            const imgId = img.id.toLowerCase();
-
-            if (imgId === category) {
-                item.style.display = "block";
-            } else {
-                item.style.display = "none";
-            }
-        });
-    })
-})
